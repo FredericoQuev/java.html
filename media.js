a54a379989222f4d98fcc8122ac859f73b08472e
FredericoQuev <105942058+FredericoQuev@users.noreply.github.com>
2023-10-11 08:51:05 -0300

    js

--- media.js
+++ media.js
@@ -1,4 +1,4 @@
-//cria referencia ao form e elementos de resposta do programa
+alert("jkniboiu")   //cria referencia ao form e elementos de resposta do programa
 
 const form = document.querySelector("form")
 const resp1 = document.querySelector("h3")
