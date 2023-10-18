430721f2ddbead80d57f5c347a00ecbc38dfc4cf
FredericoQuev <105942058+FredericoQuev@users.noreply.github.com>
2023-10-18 08:47:56 -0300

    estilo

---
+++ triangulo.js
@@ -0,0 +1,23 @@
+alert("ok")
+const form = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//ouvinte
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const permitida = Number(form.inPermitida.value)
+    const condutor = Number(form.inCondutor.value)
+    let leve = permitida + (permitida * 0.2)
+    alert(permitida+condutor+leve)
+    if(condutor <= permitida){
+        resp.innerText = `Situação Sem Multa`
+
+    } else if (condutor <= leve) {
+        resp.innerText = `Situação: Multa Leve`
+
+    } else {
+        resp.innerText = `Situação Multa Grave`
+    }
+})
